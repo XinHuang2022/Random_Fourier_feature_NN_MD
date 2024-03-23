58678103e3985ebf9417ee6140e9eaa5bdb78451
Xin Huang <huangxin06@hotmail.com>
2024-03-23 16:14:57 +0100

    Remove old pipelines

--- d1_Visualisation_and_Data_Storage/Visualise_vi_norm/dv_dx2_func.m
+++
@@ -1,6 +0,0 @@
-	function dv_dx2_value = dv_dx2_func( x1, x2, alpha, gamma, R_a, sigma_a )
-		
-		R = sqrt( x1.^2 + x2.^2 );
-		dv_dx2_value = alpha * x2 + gamma * ( x1 .* cos( x1 .* x2 ) .* chi_r_func( R, R_a, sigma_a ) + sin( x1 .* x2 ) .* dchi_dr_func( R, R_a, sigma_a ) .* x2 ./ R );
-		
-	end	
